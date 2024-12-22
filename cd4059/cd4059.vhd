1b9677f4927c6b8d7d60d1ea289bae01373748dc
bsdmry <postmaster@myghost.name>
2024-12-23 01:09:00 +0200

    Draft implementation

--- cd4059/cd4059.vhd
+++ cd4059/cd4059.vhd
@@ -8,47 +8,206 @@ entity cd4059 is
 			Ka: in std_logic;
 			Kb: in std_logic;
 			Kc: in std_logic;
-			jbus: in std_logic_vector(15 downto 0)
+			jbus: in std_logic_vector(15 downto 0);
+			output: out std_logic := '1'
 	 );
 end cd4059;
 
 architecture cd4059_arch of cd4059 is
-signal state: std_logic_vector(2 downto 0) := "000";
-signal bits: std_logic_vector(15 downto 0) := x"0000";
-signal first_cnt: std_logic_vector(3 downto 0) := "0000";
-signal int_cnt1: std_logic_vector(3 downto 0) := "0000";
-signal int_cnt2: std_logic_vector(3 downto 0) := "0000";
-signal int_cnt3: std_logic_vector(3 downto 0) := "0000";
-signal last_cnt: std_logic_vector(2 downto 0) := "000";
+	signal state: std_logic_vector(2 downto 0) := "000";
+	signal statediv: std_logic_vector(2 downto 0) := "000";
+	signal counter1_buf: std_logic_vector(3 downto 0) := "0000";
+	signal counter2_buf: std_logic_vector(3 downto 0) := "0000";
+	signal counter3_buf: std_logic_vector(3 downto 0) := "0000";
+	signal counter4_buf: std_logic_vector(3 downto 0) := "0000";
+	signal counter5_buf: std_logic_vector(2 downto 0) := "000";
+	signal counter1: std_logic_vector(3 downto 0) := "0000";
+	signal counter2: std_logic_vector(3 downto 0) := "0000";
+	signal counter3: std_logic_vector(3 downto 0) := "0000";
+	signal counter4: std_logic_vector(3 downto 0) := "0000";
+	signal counter5: std_logic_vector(2 downto 0) := "000";
+	signal divider: natural range 0 to 9 := 9; --can be 2, 4, 5, 8, 10 (combination Ka, Kb, Kc)
+
 begin
-	process(clk) begin
+	process(clk) 
+		variable divcnt: natural range 0 to 9 := 0;
+		variable c1000: natural range 0 to 999 := 999;
+		variable c100: natural range 0 to 99 := 99;
+		variable c10: natural range 0 to 9 := 9;
+		begin
 		if rising_edge(clk) then
 			if (Kb = '0' and Kc = '0') then
-				bits <=	jbus;
 				state <= "000";
 			else
 				case state is
 					when "000" =>
-						if (Ka = '1' and Kb = '1' and Kc = '1') then
-							first_cnt(0) <= jbus(0);
-							last_cnt <= jbus(3 downto 1);	
-						elsif (Ka = '0' and Kb = '1' and Kc = '1') then
-							first_cnt(1 downto 0) <= jbus(1 downto 0);
-							last_cnt(1 downto 0) <= jbus(3 downto 2);	
-						elsif (Ka = '0' and Kb = '0' and Kc = '1') then
-							first_cnt(2 downto 0) <= jbus(2 downto 0);
-							last_cnt(0) <= jbus(3);	
-						elsif (Ka = '1' and Kb = '1' and Kc = '0') then
-							first_cnt <= jbus(3 downto 0);
-							last_cnt <= "000";	
+						if (Ka = '1' and Kb = '1' and Kc = '1') then -- /2 mode
+							counter1(0) <= jbus(0);
+							counter1(1) <= '0';
+							counter1(2) <= '0';
+							counter1(3) <= '0';
+							counter5(0) <= jbus(1);
+							counter5(1) <= jbus(2);
+							counter5(2) <= jbus(3);
+
+							counter1_buf(0) <= jbus(0);
+							counter1_buf(1) <= '0';
+							counter1_buf(2) <= '0';
+							counter1_buf(3) <= '0';
+							counter5_buf(0) <= jbus(1);
+							counter5_buf(1) <= jbus(2);
+							counter5_buf(2) <= jbus(3);
+
+							divider <= 1; divcnt := 1; 	
+							state <= "010";
+						elsif (Ka = '0' and Kb = '1' and Kc = '1') then -- /4 mode
+							counter1(0) <= jbus(0);
+							counter1(1) <= jbus(1);
+							counter1(2) <= '0';
+							counter1(3) <= '0';
+							counter5(0) <= jbus(2);
+							counter5(1) <= jbus(3);
+							counter5(2) <= '0';
+
+							counter1_buf(0) <= jbus(0);
+							counter1_buf(1) <= jbus(1);
+							counter1_buf(2) <= '0';
+							counter1_buf(3) <= '0';
+							counter5_buf(0) <= jbus(2);
+							counter5_buf(1) <= jbus(3);
+							counter5_buf(2) <= '0';
+
+							divider <= 3; divcnt := 3;	
+							state <= "010";
+						elsif (Ka = '1' and Kb = '0' and Kc = '1') then -- /5 mode
+							counter1(0) <= jbus(0);
+							counter1(1) <= jbus(1);
+							counter1(2) <= jbus(2);
+							counter1(3) <= '0';
+							counter5(0) <= jbus(3);
+							counter5(1) <= '0';
+							counter5(2) <= '0';
+
+							counter1_buf(0) <= jbus(0);
+							counter1_buf(1) <= jbus(1);
+							counter1_buf(2) <= jbus(2);
+							counter1_buf(3) <= '0';
+							counter5_buf(0) <= jbus(3);
+							counter5_buf(1) <= '0';
+							counter5_buf(2) <= '0';
+
+							divider <= 4; divcnt := 4;	
+							state <= "010";
+						elsif (Ka = '0' and Kb = '0' and Kc = '1') then -- /8 mode
+							counter1(0) <= jbus(0);
+							counter1(1) <= jbus(1);
+							counter1(2) <= jbus(2);
+							counter1(3) <= '0';
+							counter5(0) <= jbus(3);
+							counter5(1) <= '0';
+							counter5(2) <= '0';
+
+							counter1_buf(0) <= jbus(0);
+							counter1_buf(1) <= jbus(1);
+							counter1_buf(2) <= jbus(2);
+							counter1_buf(3) <= '0';
+							counter5_buf(0) <= jbus(3);
+							counter5_buf(1) <= '0';
+							counter5_buf(2) <= '0';
+
+							divider <= 7; divcnt := 7; 	
+							state <= "010";
+						elsif (Ka = '1' and Kb = '1' and Kc = '0') then -- /10 mode
+							counter1(0) <= jbus(0);
+							counter1(1) <= jbus(1);
+							counter1(2) <= jbus(2);
+							counter1(3) <= jbus(3);
+							counter5 <= "000";	
+
+							counter1_buf(0) <= jbus(0);
+							counter1_buf(1) <= jbus(1);
+							counter1_buf(2) <= jbus(2);
+							counter1_buf(3) <= jbus(3);
+							counter5_buf <= "000";	
+
+							divider <= 9; divcnt := 9;
+							state <= "010";
 						end if;
-						int_cnt1 <= jbus(7 downto 4);
-						int_cnt2 <= jbus(11 downto 8);
-						int_cnt3 <= jbus(15 downto 12);
+						counter2_buf <= jbus(7 downto 4);
+						counter3_buf <= jbus(11 downto 8);
+						counter4_buf <= jbus(15 downto 12);
+						counter2 <= jbus(7 downto 4);
+						counter3 <= jbus(11 downto 8);
+						counter4 <= jbus(15 downto 12);
+					when "010" =>  -- Count down for [Nmode] * [1000 x 5th decade + 100 x 4th decade + 10 x 3rd decade + 1 x 2nd decade]
+						output <= '0';
+						if divcnt = 0 then -- Nmode
+							divcnt := divider;
+							case statediv is
+								when "000" =>	
+									if counter5 = "000" then --5th decade
+										statediv <= "001";
+									else
+										if c1000 = 0 then -- 1000 x
+											counter5 <= std_logic_vector(unsigned(counter5) - 1);
+											c1000 := 999;
+										else
+											c1000 := c1000 - 1;
+										end if;
+									end if;
+								when "001" =>
+									if counter4 = "0000" then --4th decade
+										statediv <= "010";
+									else
+										if c100 = 0 then -- 100 x
+											counter4 <= std_logic_vector(unsigned(counter4) - 1);
+											c100 := 99;
+										else
+											c100 := c100 - 1;
+										end if;
+									end if;
+								when "010" =>
+									if counter3 = "0000" then --3th decade
+										statediv <= "011";
+									else
+										if c10 = 0 then -- 10 x
+											counter3 <= std_logic_vector(unsigned(counter3) - 1);
+											c10 := 9;
+										else
+											c10 := c10 - 1;
+										end if;
+									end if;
+								when "011" =>
+									if counter2 = "0000" then
+										statediv <= "000";
+										state <= "011";
+									else
+										counter2 <= std_logic_vector(unsigned(counter2) - 1);
+									end if;
+								when others =>
+							end case;			
+						else
+							divcnt := divcnt - 1;
+						end if;	
+					when "011" => -- Count down 1st decade
+						if counter1 = "0000" then
+							counter1 <= counter1_buf;
+							counter2 <= counter2_buf;
+							counter3 <= counter3_buf;
+							counter4 <= counter4_buf;
+							counter5 <= counter5_buf;
+
+							state <= "010"; --Count again
+							output <= '1';
+						else
+							counter1 <= std_logic_vector(unsigned(counter1) - 1);
+						end if;
+						
 					when others => 
 				end case;	
 			end if;
 		end if;
 	end process;
+
 end cd4059_arch;
 
--- cd4059/cd4059_tb.vhd
+++ cd4059/cd4059_tb.vhd
@@ -8,18 +8,41 @@ end cd4059_tb;
 
 architecture cd4059_tb_arch of cd4059_tb is
 component cd4059 is port (
-	clk : in std_logic
+	clk : in std_logic;
+	Ka: in std_logic;
+	Kb: in std_logic;
+	Kc: in std_logic;
+	jbus: in std_logic_vector(15 downto 0);
+	output: out std_logic
 	);
 end component;
 
 signal clk:  STD_LOGIC := '1';
 signal sym_stop: STD_LOGIC := '0';
 constant clk_period : time := 50 ns;
+signal ka_pin: std_logic := '0';
+signal kb_pin: std_logic := '0';
+signal kc_pin: std_logic := '0';
+signal jam_input1_4: std_logic_vector(3 downto 0) := "0000";
+signal jam_input5_8: std_logic_vector(3 downto 0) := "0000";
+signal jam_input9_12: std_logic_vector(3 downto 0) := "0000";
+signal jam_input13_16: std_logic_vector(3 downto 0) := "0000";
+signal output: std_logic;
+signal measure: natural range 0 to 65535 := 0;
 
 begin
 --	UNIT UNDER TEST
 uut: cd4059 port map (
-	clk => clk
+	clk => clk,
+	Ka => ka_pin,
+	Kb => kb_pin,
+	Kc => kc_pin,
+	jbus(3 downto 0) => jam_input1_4,
+	jbus(7 downto 4) => jam_input5_8,
+	jbus(11 downto 8) => jam_input9_12,
+	jbus(15 downto 12) => jam_input13_16,
+	output => output
+	
 );
 --	CLK GENERATOR
 clk_process : process 
@@ -36,11 +59,44 @@ end process;
 --	TEST SIGNALS
 stim_process : process
 begin
-	wait for 30 ns;
-	-- INSERT TEST CODE HERE ---
+	wait for 300 ns;
+	-- Setup divison by 8471
+	-- Prescale clock 5 (N = 5)
+	-- 8471 / 5 = 1694,2
+	-- 
+	-- Preset values  1694 and 2
+	--
+	-- J1  J2  J3  J4     J5  J6  J7  J8     J9  J10 J11 J12    J13 J14 J15 J16
+	--  0   1   2   3      0   1   2   3 	  0   1   2   3      0   1   2   3
+	-- |_________| |_|   |______________|    |_____________|    |_____________|
+	--      2       1           4                   9                  6
+	--
+	--  0   1   0   1      0   0   1   0      1   0   0   1      0   1   1   0
+
+	jam_input1_4 <= "1010";
+	jam_input5_8 <= "0100";
+	jam_input9_12 <= "1001";
+	jam_input13_16 <= "0110";
+	wait for clk_period;
+	ka_pin <= '1';
+	kb_pin <= '0';
+	kc_pin <= '1';
+	wait for 1 ms;
+	
 	report "end of test" severity note;
 	sym_stop <= '1';
 	wait;
+end process;
+
+process(clk)
+	begin
+	if rising_edge(clk) then
+		if output = '0' then
+			measure <= measure +1;
+		else
+			measure <= 0;
+		end if;
+	end if;
 end process;
 
 end cd4059_tb_arch;
