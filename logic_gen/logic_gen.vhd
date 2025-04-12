9fa78c48828f0458052b1da5f92d5019e033e609
bsdmry <postmaster@myghost.name>
2025-04-12 21:10:12 +0300

    Fix N-div divider

--- logic_gen/logic_gen.vhd
+++ logic_gen/logic_gen.vhd
@@ -1,9 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
-library cmos_logic;
-use cmos_logic.cd4020;
-
 
 entity logic_gen is
     Port ( 
@@ -15,21 +12,28 @@ entity logic_gen is
 end logic_gen;
 
 architecture logic_gen_arch of logic_gen is
-signal limit: std_logic_vector(11 downto 0) := "000000000011";
+signal limit: std_logic_vector(11 downto 0) := "000000000010";
 signal cntval: std_logic_vector(11 downto 0);
 signal equal: std_logic := '1';
+signal outclk: std_logic := '0';
+
+component cd4020 is
+    Port ( 
+		clk : in  STD_LOGIC;
+		rst: in std_logic;
+		q: out std_logic_vector(11 downto 0)
+	 );
+end component;
 
 begin
-counter: cd4020 port map (q => cntval, clk => clk, rst => equal);
-	process(clk) begin
-		if rising_edge(clk) then
-			if limit = cntval then
-				equal <= '0';
-				divclk <= divclk xor '1';
-			else
-				equal <= '1';
-			end if;
+	counter: cd4020 port map (q => cntval, clk => clk, rst => equal);
+
+	equal <= '0' when unsigned(limit) = unsigned(cntval) else '1';
+	process(equal) begin
+		if falling_edge(equal) then
+			outclk <= outclk xor '1';
 		end if;
 	end process;
+	divclk <= outclk;
 end logic_gen_arch;
 
