dbb21ea3d1afbacacc237f14fbd0d827fc0e2c8c
bsdmry <postmaster@myghost.name>
2025-04-19 22:17:48 +0300

    CD4020: Synth check

--- cd4020/cd4020.vhd
+++ cd4020/cd4020.vhd
@@ -14,14 +14,12 @@ end cd4020;
 architecture cd4020_arch of cd4020 is
 begin
 	process(clk)
-variable c: std_logic_vector(11 downto 0) := "000000000000";
-begin
-		if rst = '1' then
-			if rising_edge(clk) then
-				c := std_logic_vector( unsigned(c) + 1);
-			end if;
-		else
+		variable c: std_logic_vector(11 downto 0) := "000000000000";
+		begin
+		if rst = '0' then
 			c := "000000000000";
+		elsif falling_edge(clk) then
+			c := std_logic_vector(unsigned(c) + 1);
 		end if;
 		q <= c;
 	end process;
