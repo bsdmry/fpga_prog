9b8a88776d4df77fed87fc92bc1899a14f7e1687
bsdmry <postmaster@myghost.name>
2025-04-20 17:20:54 +0300

    Adder/substractor update

--- add_sub/add_sub.vhd
+++ add_sub/add_sub.vhd
@@ -44,7 +44,8 @@ entity add_sub4 is
 		mode : in std_logic; -- '0' - add/ '1' - sub
 		a: in std_logic_vector(3 downto 0);
 		b: in std_logic_vector(3 downto 0);
-		carry: out std_logic;
+		carryin: in std_logic;
+		carryout: out std_logic;
 		result: out std_logic_vector(3 downto 0)
 	 );
 end add_sub4;
@@ -53,10 +54,10 @@ architecture add_sub4_arch of add_sub4 is
 signal co0, co1, co2: std_logic;
 component fulladder is port(a: in std_logic;  b: in std_logic; mode: in std_logic; carryin: in std_logic; sum: out std_logic; carryout: out std_logic); end component;
 begin
-f0: fulladder port map(a=>a(0), b=>b(0), mode=>mode, carryin=>mode, carryout=>co0, sum=>result(0));
+f0: fulladder port map(a=>a(0), b=>b(0), mode=>mode, carryin=>carryin, carryout=>co0, sum=>result(0));
 f1: fulladder port map(a=>a(1), b=>b(1), mode=>mode, carryin=>co0, carryout=>co1, sum=>result(1));
 f2: fulladder port map(a=>a(2), b=>b(2), mode=>mode, carryin=>co1, carryout=>co2, sum=>result(2));
-f3: fulladder port map(a=>a(3), b=>b(3), mode=>mode, carryin=>co2, carryout=>carry, sum=>result(3));
+f3: fulladder port map(a=>a(3), b=>b(3), mode=>mode, carryin=>co2, carryout=>carryout, sum=>result(3));
 end add_sub4_arch;
 
 library IEEE;
@@ -64,12 +65,14 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 --https://abidhussainatisp.wordpress.com/wp-content/uploads/2016/10/lecture-11-dld-bcd-adders-and-comparators.pdf
+-- SUBSTRACTION DO NOT WORK!
 entity add_sub_bcd is
     port ( 
 		mode : in std_logic; -- '0' - add/ '1' - sub
 		a: in std_logic_vector(3 downto 0);
 		b: in std_logic_vector(3 downto 0);
-		carry: out std_logic;
+		carryin: in std_logic;
+		carryout: out std_logic;
 		result: out std_logic_vector(3 downto 0)
 	 );
 end add_sub_bcd;
@@ -81,13 +84,14 @@ component add_sub4 is port (
 	mode : in std_logic; 
 	a: in std_logic_vector(3 downto 0); 
 	b: in std_logic_vector(3 downto 0); 
-	carry: out std_logic; 
+	carryin: in std_logic; 
+	carryout: out std_logic; 
 	result: out std_logic_vector(3 downto 0)); end component;
 begin
-adder1: add_sub4 port map(mode=> mode, a=>a, b=>b, carry=>bin_carry, result=>pre);
-carry <= dec_carry;
+adder1: add_sub4 port map(mode=> mode, a=>a, b=>b, carryin=>carryin, carryout=>bin_carry, result=>pre);
+carryout <= dec_carry;
 dec_carry <= bin_carry or and1_out or and2_out;
 and1_out <= pre(2) and pre(3);
 and2_out <= pre(1) and pre(3);
-adder2: add_sub4 port map(mode=>'0', a=>pre, b(0)=>'0', b(1)=>dec_carry, b(2)=>dec_carry, b(3)=>'0', result=>result);
+adder2: add_sub4 port map(mode=>'0', a=>pre, b(0)=>'0', b(1)=>dec_carry, b(2)=>dec_carry, b(3)=>'0', carryin=>'0', result=>result);
 end add_sub_bcd_arch;
--- add_sub/add_sub_tb.vhd
+++ add_sub/add_sub_tb.vhd
@@ -11,9 +11,20 @@ component add_sub4 is port (
 		mode : in std_logic; -- '0' - add/ '1' - sub
 		a: in std_logic_vector(3 downto 0);
 		b: in std_logic_vector(3 downto 0);
-		carry: out std_logic;
+		carryin: in std_logic;
+		carryout: out std_logic;
 		result: out std_logic_vector(3 downto 0)
 	);
+end component;
+component add_sub_bcd is
+    port ( 
+		mode : in std_logic; -- '0' - add/ '1' - sub
+		a: in std_logic_vector(3 downto 0);
+		b: in std_logic_vector(3 downto 0);
+		carryin: in std_logic;
+		carryout: out std_logic;
+		result: out std_logic_vector(3 downto 0)
+	 );
 end component;
 
 signal clk:  STD_LOGIC := '1';
@@ -21,8 +32,11 @@ signal sym_stop: STD_LOGIC := '0';
 constant clk_period : time := 50 ns;
 
 signal op1, op2, r: std_logic_vector(3 downto 0);
-signal op_type: std_logic := '0'; --sum
-signal carry_flag: std_logic; --sum
+signal opl1, opl2, rl: std_logic_vector(7 downto 0);
+signal op_bcd1_0, op_bcd1_1, op_bcd2_0, op_bcd2_1, r_bcd_0, r_bcd_1: std_logic_vector(3 downto 0);
+signal op_type, op_type_bcd: std_logic := '0'; --sum
+signal carry_flag, carry_flag_bcd0, carry_flag_bcd1, carry_flag_l1, carry_flag_l2: std_logic; --sum
+
 begin
 --	UNIT UNDER TEST
 uut: add_sub4 port map (
@@ -30,8 +44,44 @@ uut: add_sub4 port map (
 	a => op1,
 	b=>op2,
 	result=>r,
-	carry=>carry_flag
+	carryin=>op_type,
+	carryout=>carry_flag
 );
+
+uut1: add_sub_bcd port map (
+	mode => op_type_bcd,
+	a => op_bcd1_0,
+	b => op_bcd2_0,
+	result => r_bcd_0,
+	carryin => op_type_bcd,
+	carryout => carry_flag_bcd0
+);
+uut2: add_sub_bcd port map (
+	mode => op_type_bcd,
+	a => op_bcd1_1,
+	b => op_bcd2_1,
+	result => r_bcd_1,
+	carryin => carry_flag_bcd0,
+	carryout => carry_flag_bcd1
+);
+uutl1: add_sub4 port map (
+	mode => op_type,
+	a => opl1(3 downto 0),
+	b=>opl2(3 downto 0),
+	result=>rl(3 downto 0),
+	carryin => op_type,
+	carryout=>carry_flag_l1
+);
+uutl2: add_sub4 port map (
+	mode => op_type,
+	a => opl1(7 downto 4),
+	b=>opl2(7 downto 4),
+	result=>rl(7 downto 4),
+	carryin => carry_flag_l1,
+	carryout=>carry_flag_l2
+);
+
+
 --	CLK GENERATOR
 clk_process : process 
 begin
@@ -59,13 +109,59 @@ begin
 	op2<=std_logic_vector(to_unsigned(6, 4));
 	wait for 30 ns;
 	op1<=std_logic_vector(to_unsigned(2, 4));
-	op2<=std_logic_vector(to_unsigned(5, 4));
-	
+	op2<=std_logic_vector(to_unsigned(5, 4));	
 
 	-- INSERT TEST CODE HERE ---
 	report "end of test" severity note;
 	sym_stop <= '1';
 	wait;
+end process;
+
+stim_process_l : process
+begin
+	wait for 30 ns;
+	opl1<=std_logic_vector(to_unsigned(101, 8));
+	opl2<=std_logic_vector(to_unsigned(13, 8));
+	wait for 30 ns;
+	opl1<=std_logic_vector(to_unsigned(90, 8));
+	opl2<=std_logic_vector(to_unsigned(120, 8));
+	wait for 30 ns;
+	opl1<=std_logic_vector(to_unsigned(231, 8));
+	opl2<=std_logic_vector(to_unsigned(77, 8));
+	wait for 30 ns;
+	opl1<=std_logic_vector(to_unsigned(150, 8));
+	opl2<=std_logic_vector(to_unsigned(150, 8));	
+	wait;
+end process;
+
+stim2_process : process
+begin
+	wait for 30 ns;
+	op_bcd1_0<=std_logic_vector(to_unsigned(5, 4));
+	op_bcd1_1<=std_logic_vector(to_unsigned(2, 4)); --25 
+	-- PLUS
+	op_bcd2_0<=std_logic_vector(to_unsigned(3, 4));
+	op_bcd2_1<=std_logic_vector(to_unsigned(0, 4)); --3
+	wait for 30 ns;
+	op_bcd1_0<=std_logic_vector(to_unsigned(4, 4));
+	op_bcd1_1<=std_logic_vector(to_unsigned(1, 4)); --14 
+	-- PLUS
+	op_bcd2_0<=std_logic_vector(to_unsigned(9, 4));
+	op_bcd2_1<=std_logic_vector(to_unsigned(1, 4)); --19
+	wait for 30 ns;
+	op_type_bcd<='1'; --substraction
+	op_bcd1_0<=std_logic_vector(to_unsigned(3, 4));
+	op_bcd1_1<=std_logic_vector(to_unsigned(4, 4)); --43
+	-- MINUS
+	op_bcd2_0<=std_logic_vector(to_unsigned(8, 4));
+	op_bcd2_1<=std_logic_vector(to_unsigned(0, 4)); --8
+	wait for 30 ns;
+	op_bcd1_0<=std_logic_vector(to_unsigned(1, 4));
+	op_bcd1_1<=std_logic_vector(to_unsigned(3, 4)); --31
+	-- MINUS
+	op_bcd2_0<=std_logic_vector(to_unsigned(1, 4));
+	op_bcd2_1<=std_logic_vector(to_unsigned(3, 4)); --31
+wait;
 end process;
 
 end add_sub_tb_arch;
