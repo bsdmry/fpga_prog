74d440e77a9b73729d9e3effb246d6e58b18a16e
Igor Volodin <postmaster@myghost.name>
2025-04-23 18:12:48 +0300

    CD4543 Fix

--- cd4543/cd4543.vhd
+++ cd4543/cd4543.vhd
@@ -30,24 +30,38 @@ component bcd2sseg is
 	 );
 end component;
 signal inbcd: std_logic_vector(3 downto 0) := "0000";
+signal check: std_logic_vector(3 downto 0) := "0000";
 signal oseg: std_logic_vector(6 downto 0) := "0000000";
-signal change: std_logic;
+signal change: std_logic := '0';
+signal det: std_logic := '0';
 begin
 	bcdconv: bcd2sseg port map(
 			b0=> inbcd(0), b1=> inbcd(1), b2=> inbcd(2), b3=> inbcd(3),
 			a=>oseg(0),b=>oseg(1),c=>oseg(2),d=>oseg(3),
 			e=>oseg(4),f=>oseg(5),g=>oseg(6));
 	
-	q(0) <= (not bl and ph) or (ph and not oseg(0)) or (bl and not ph and oseg(0));
-	q(1) <= (not bl and ph) or (ph and not oseg(1)) or (bl and not ph and oseg(1));
-	q(2) <= (not bl and ph) or (ph and not oseg(2)) or (bl and not ph and oseg(2));
-	q(3) <= (not bl and ph) or (ph and not oseg(3)) or (bl and not ph and oseg(3));
-	q(4) <= (not bl and ph) or (ph and not oseg(4)) or (bl and not ph and oseg(4));
-	q(5) <= (not bl and ph) or (ph and not oseg(5)) or (bl and not ph and oseg(5));
-	q(6) <= (not bl and ph) or (ph and not oseg(6)) or (bl and not ph and oseg(6));
-	change <= (d(0) xor inbcd(0)) or (d(1) xor inbcd(1)) or (d(2) xor inbcd(2)) or (d(3) xor inbcd(3));
+	-- seg    blank     phase    out
+	--  1     0         0	     1
+	--  1     0         1        0
+	--  1     1         0        0
+	--  1     1         1        1
+	--  0     0         0        0
+	--  0     0         1        1
+	--  0     1         0        0
+	--- 0     1         1        1
+	
+	q(0) <= (not oseg(0) and ph) or (bl and ph) or (oseg(0) and not bl and not ph);
+	q(1) <= (not oseg(1) and ph) or (bl and ph) or (oseg(1) and not bl and not ph);
+	q(2) <= (not oseg(2) and ph) or (bl and ph) or (oseg(2) and not bl and not ph);
+	q(3) <= (not oseg(3) and ph) or (bl and ph) or (oseg(3) and not bl and not ph);
+	q(4) <= (not oseg(4) and ph) or (bl and ph) or (oseg(4) and not bl and not ph);
+	q(5) <= (not oseg(5) and ph) or (bl and ph) or (oseg(5) and not bl and not ph);
+	q(6) <= (not oseg(6) and ph) or (bl and ph) or (oseg(6) and not bl and not ph);
+	change <= (d(0) xor check(0)) or (d(1) xor check(1)) or (d(2) xor check(2)) or (d(3) xor check(3));
 	process(change) begin
 		if rising_edge(change) then
+			det <= det xor '1';
+			check <= d;
 			if le_n = '1' then
 				inbcd <= d;
 			end if;
