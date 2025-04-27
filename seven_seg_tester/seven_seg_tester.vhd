26648f85e06a33a0925be235a007eee3cf5a253d
bsdmry <postmaster@myghost.name>
2025-04-28 00:52:52 +0300

    Button handle draft

--- seven_seg_tester/seven_seg_tester.vhd
+++ seven_seg_tester/seven_seg_tester.vhd
@@ -5,6 +5,7 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity seven_seg_tester is
     Port ( clk : in  std_logic;
+		   frq_chg_in: in std_logic;
     	   segs: out std_logic_vector(7 downto 0);
     	   pos: out std_logic_vector(3 downto 0)
 	 );
@@ -26,20 +27,41 @@ component cd4543 is
     port ( 
 		le_n : in std_logic; d: in std_logic_vector(3 downto 0);
 		ph: in std_logic; bl: in std_logic;
-		q: out std_logic_vector(6 downto 0)
-	 );
+		q: out std_logic_vector(6 downto 0));
 end component;
 
 component cd74hc153 is
     port ( 
-	mux0in : in std_logic_vector(3 downto 0); mux1in : in std_logic_vector(3 downto 0);
-    s0: in std_logic; s1: in std_logic; en0_n: in std_logic; en1_n: in std_logic;
-    out0: out std_logic; out1: out std_logic);
+		mux0in : in std_logic_vector(3 downto 0); mux1in : in std_logic_vector(3 downto 0);
+    	s0: in std_logic; s1: in std_logic; en0_n: in std_logic; en1_n: in std_logic;
+    	out0: out std_logic; out1: out std_logic);
 end component;
 
+component cd4029 is
+    Port ( 
+	clk : in  STD_LOGIC;
+    	bindec: in std_logic;
+    	updown: in std_logic;
+    	preset_en: in std_logic;
+    	carry_in: in std_logic;
+    	jam: in std_logic_vector(3 downto 0);
+    	output: out std_logic_vector(3 downto 0);
+    	carry_out: out std_logic
+	 );
+end component;
 
+component cd74hc74_async_rst is
+    port (
+		clk0: in std_logic; d0 : in std_logic;
+		r0_n: in std_logic; s0_n: in std_logic;
+		q0: out std_logic := '0'; q0_n: out std_logic;
+		clk1: in std_logic; d1 : in std_logic;
+		r1_n: in std_logic; s1_n: in std_logic;
+		q1: out std_logic := '0'; q1_n: out std_logic
+	 );
+end component;
 
-signal clk1kh, clk1h: std_logic;
+signal clk1kh, clk10h, clk1h: std_logic;
 signal bcd0: std_logic_vector(3 downto 0) := "0011";
 signal bcd1: std_logic_vector(3 downto 0) := "0010";
 signal bcd2: std_logic_vector(3 downto 0) := "0001"; 
@@ -47,10 +69,14 @@ signal bcd3: std_logic_vector(3 downto 0) := "0000";
 signal active_bcd: std_logic_vector(3 downto 0) := "0000";
 signal active_dig: std_logic_vector(1 downto 0) := "00";
 
+signal frq_chng: std_logic;
+signal bcd0_crry, bcd1_crry, bcd2_crry, bcd3_crry: std_logic;
+signal frq_chng_dir: std_logic := '1';
+
 signal inc: std_logic_vector(3 downto 0) := "0000";
 
 begin
-	clk_src: gen10clock generic map(CLK_MHZ => 27) port map(clk=>clk, c1k=>clk1kh, c1=>clk1h);
+	clk_src: gen10clock generic map(CLK_MHZ => 27) port map(clk=>clk, c1k=>clk1kh, c10=>clk10h, c1=>clk1h);
 	bcdmux0: cd74hc153 port map(
 		mux0in(0)=>bcd0(0), mux0in(1)=>bcd1(0), mux0in(2)=>bcd2(0), mux0in(3)=>bcd3(0), 
 		mux1in(0)=>bcd0(1), mux1in(1)=>bcd1(1), mux1in(2)=>bcd2(1), mux1in(3)=>bcd3(1),
@@ -66,16 +92,37 @@ begin
 	bcd2ss: cd4543 port map(le_n => '1', d=>active_bcd, ph=>'0', bl=>'0', q=>segs(6 downto 0));
 	segs(7) <= '1';
 
-	process(clk1h) 
-		begin
-		if rising_edge(clk1h) then
-			case inc is
-				when "1001" => inc <= "0000";
-				when others => inc <= std_logic_vector(unsigned(inc)+1);
-			end case;
-			bcd0 <= inc;
-		end if;
-	end process;
+	u_bcd0: cd4029 port map (
+    	clk => frq_chng, bindec => '0', updown => frq_chng_dir, preset_en => '0',
+    	carry_in => '0', jam => "0000", output =>bcd0,
+    	carry_out => bcd0_crry);
+	u_bcd1: cd4029 port map (
+    	clk => frq_chng, bindec => '0', updown => frq_chng_dir, preset_en => '0',
+    	carry_in => bcd0_crry, jam => "0000", output =>bcd1,
+    	carry_out => bcd1_crry);
+	u_bcd2: cd4029 port map (
+    	clk => frq_chng, bindec => '0', updown => frq_chng_dir, preset_en => '0',
+    	carry_in => bcd1_crry, jam => "0000", output =>bcd2,
+    	carry_out => bcd2_crry);
+	u_bcd3: cd4029 port map (
+    	clk => frq_chng, bindec => '0', updown => frq_chng_dir, preset_en => '0',
+    	carry_in => bcd2_crry, jam => "0000", output =>bcd3,
+    	carry_out => bcd3_crry);
+
+	debncr0: cd74hc74_async_rst port map(
+			clk0=>clk10h, d0=>frq_chg_in, r0_n=>'1', s0_n=>'1', q0=>frq_chng,
+			clk1=>clk10h, d1=>'0', r1_n=>'1', s1_n=>'1');
+
+	--process(clk1h) 
+	--	begin
+	--	if rising_edge(clk1h) then
+	--		case inc is
+	--			when "1001" => inc <= "0000";
+	--			when others => inc <= std_logic_vector(unsigned(inc)+1);
+	--		end case;
+	--		bcd0 <= inc;
+	--	end if;
+	--end process;
 
 	process(clk1kh) begin
 		if rising_edge(clk1kh) then
